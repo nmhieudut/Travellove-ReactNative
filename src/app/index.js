b121bfeacf0570fdd4bf5ee31d13817d66d232c1
tuanconbu <hieutk5@gmail.com>
2020-11-11 21:26:29 +0700

    update stack

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text} from 'react-native';
 
 export default function App() {
   return (
     <View>
-      <Text>Hello con cac</Text>
+      <Text>Hello cac con vo</Text>
     </View>
-  )
+  );
 }
