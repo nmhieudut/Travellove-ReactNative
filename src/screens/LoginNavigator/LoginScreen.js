ed480dc6828548379ef5b182cf728f74950a1e4e
tuanconbu <hieutk5@gmail.com>
2020-11-12 22:18:32 +0700

    login screen

--- src/screens/LoginNavigator/LoginScreen.js
+++ src/screens/LoginNavigator/LoginScreen.js
@@ -1,10 +1,51 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import mainImage from '../../assets/image.jpg';
 
+const heightScr = Dimensions.get('screen').height;
+const widthScr = Dimensions.get('screen').width;
 export default function LoginScreen() {
-     return (
-          <View>
-               <Text>Login</Text>
-          </View>
-     )
+  return (
+    <View style={styles.container}>
+      {/* <ImageBackground style={styles.backgroundImage} source={mainImage}> */}
+      <View styles={styles.welcome}>
+        <Text styles={styles.title}>Welcome</Text>
+      </View>
+      <View styles={styles.formArea}>
+        <Text>Form</Text>
+      </View>
+      {/* </ImageBackground> */}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '75%',
+    opacity: 0.7,
+  },
+  welcome: {
+    height: heightScr / 3,
+    backgroundColor: 'red',
+  },
+  title: {
+    marginTop: 30,
+    backgroundColor: 'red',
+  },
+  formArea: {
+    flex: 4,
+  },
+});
